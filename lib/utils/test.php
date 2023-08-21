3420ca82cc6a3d2bb3b5fa64fd35a579f64f3914
John Lusumpa <o.lusumpa@gmail.com>
2023-08-21 12:36:39 +0200

    Encryption test scripts working (Dart/PHP)

--- lib/utils/test.php
+++ lib/utils/test.php
@@ -1,25 +1,10 @@
 <?php
-echo "<pre>\n";
-$key = base64_decode("G0HPTE61KCQ+CYn3voqMlFnXEtpaow6gYDqaaGSVzuE=");
-$textToEncrypt = "Hello world! This my secret message.";
-$iv = base64_decode('b3902f0a5e90b20577fcc62e');
-echo "BASE64(IV)=" . base64_encode($iv) . "\n";
-$encrypted = openssl_encrypt($textToEncrypt, 'aes-256-cbc', $key, 0, $iv);
-echo "encrypted output=" . $encrypted . "\n";
-//  PART 2. DECRYPT - do the reverse
-$decrypted = openssl_decrypt($encrypted,  'aes-256-cbc', $key, 0, $iv);
-echo "decrypted output=" . $decrypted . "\n";
- 
-
-
-
-
-echo "\n" . "Reference test with fixed IV" . "\n";
-$iv = base64_decode("cJrccDraCqm7rQXdOsS8Zg==");
-echo "BASE64(IV)=" . base64_encode($iv) . "\n";
-$encrypted = openssl_encrypt($textToEncrypt, 'aes-256-cbc', $key, 0, $iv);
-echo "encrypted output=" . $encrypted . "\n";
-echo "expected output =" . "p+aQDK8isX68i+PPl4uhsYW2sJFR40a+nbnj29wd2TN1mnvWmiI4EU12CsRWlEp0" . "\n";
-$decrypted = openssl_decrypt($encrypted,  'aes-256-cbc', $key, 0, $iv);
-echo "decrypted output=" . $decrypted . "\n";
-echo "</pre>\n";
+$ciphering = "AES-128-CBC";
+$options = 0;
+$simple_string = 'ZMPfMnSbw0KFpJqT0TcI/BBbDl8M7RrrCYVqc2xAlv/nz00Hl1TzCYAUazzuthGAgFmaZAcNNcol4KKXAcNJwA==';
+$decryption_iv = 'YoZr16CharactXrK';
+$decryption_key = "YoZr16CharactXrK";
+$decryption=openssl_decrypt ($simple_string, $ciphering,
+		$decryption_key, $options, $decryption_iv);
+echo $decryption;
+?>
